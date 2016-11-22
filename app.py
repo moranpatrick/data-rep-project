7e2d7e14aabbef78b03147fb2e6b7acfd11920b5
Patrick Moran <Patrick Moran>
2016-11-22 18:39:39 +0000

    Added routes to app.py to render templates

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>HomePage<h1>'
+    return render_template("homePage.html")
 
+@app.route('/form')
+def formPage():
+    return render_template("form.html")
+
+@app.route('/forum')
+def forum():
+    return render_template("forum.html")
 
 if __name__ == '__main__':
     app.run()

8dfeb57d06e182e5c478b1d580b48c6d76716551
gerardnaughton7 <gerardnaughton7@gmail.com>
2016-11-24 16:43:58 +0000

    Created Couchdb database and imported it to app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+#Couchdb import
+import couchdb
+couch = couchdb.Server("http://localhost:5984")
+
+db = couch['forum']
+
 #Flask Imports
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+#save function
+def save_post(db, post):
+    post['_email'] = post['id_postStr']
+    db.save(post)
 
 @app.route('/')
 def index():

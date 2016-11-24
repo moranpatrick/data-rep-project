d48a935f7679cd36dd14bf8a4dbcdb4e76f6e61f
Patrick Moran <Patrick Moran>
2016-11-24 14:25:16 +0000

    tidied up app.py and html

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
+#Flask Imports
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
+    # When routed here render home page template
     return render_template("homePage.html")
 
 @app.route('/form')
-def formPage():
+def form():
+    #When routed here render from template
     return render_template("form.html")
 
 @app.route('/forum')
 def forum():
+    # When routed here render forum template
     return render_template("forum.html")
 
+#Run App
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
